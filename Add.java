50037402caa97d5f6bd90ca8a291b9e32c5d5eb2
Shalinigowda27 <shalinigowda1727@gamil.com>
2025-06-30 13:00:02 +0530

    java files

---
+++ Add.java
@@ -0,0 +1,8 @@
+class   Add {
+          public static void main(String nothing[])
+{
+          int num1=27;
+          int num2=17;
+          System.out.println("Add num1+num2");
+}
+}
---
+++ Aeroplane.java
@@ -0,0 +1,6 @@
+class Aeroplane{
+       public static void main(String[] args)
+{
+        System.out.println("At what time plane will take-off?");
+}          
+}
